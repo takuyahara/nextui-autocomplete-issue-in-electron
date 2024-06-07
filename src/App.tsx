69b413ef6c948c7c50e12b961b3a0ca703c7b735
takuyahara <h.taku86@gmail.com>
2024-06-07 10:13:27 +0900

    Changed import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Autocomplete, AutocompleteItem} from "@nextui-org/react";
+import {Autocomplete, AutocompleteItem} from "@nextui-org/autocomplete";
 
 
 export default function App() {
